2ee681b561e4849985e371604f8ec6b2d52e726f
Greg M <gmmurray8@gmail.com>
2025-10-27 00:18:40 -0400

    fix: more padding on home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -56,7 +56,7 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <Box>
-      <Box minH={{ base: '40vh', md: '45vh' }}>
+      <Box minH={{ base: '40vh', md: '45vh' }} mb="10" pt="4">
         <Container
           pos="relative"
           h="100%"
